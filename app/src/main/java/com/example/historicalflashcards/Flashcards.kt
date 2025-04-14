93a89314844657bac7c99bde4366bcb18a86dc96
goitsi <ST10471532@rcconnect.edu.za>
2025-04-14 14:03:02 +0000

    Creating the app and a few text features

--- app/src/main/java/com/example/historicalflashcards/Flashcards.kt
+++ app/src/main/java/com/example/historicalflashcards/Flashcards.kt
@@ -6,15 +6,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.historicalflashcards.ui.theme.HistoricalFlashcardsTheme
 
 class Flashcards : ComponentActivity() {
@@ -28,7 +35,18 @@ class Flashcards : ComponentActivity() {
                     color = Color.Transparent
                 ) {
                    
-                    Column {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+
+                        Text(
+                            text = "Historical Flashcards",
+                            modifier = Modifier,
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.Black
+                        )
+
+                        Spacer(modifier = Modifier.size(30.dp))
 
                         Row {
                             Button(
--- app/src/main/java/com/example/historicalflashcards/MainActivity.kt
+++ app/src/main/java/com/example/historicalflashcards/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.historicalflashcards.ui.theme.HistoricalFlashcardsTheme
 
@@ -35,11 +38,22 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally)
                     {
                         Text(
-                            text = "Welcome to Historical Flash Cards",
+                            text = "Historical Flashcards",
                             modifier = Modifier,
                             fontSize = 30.sp,
                             fontWeight = FontWeight.Black
                         )
+                        Spacer(modifier = Modifier.size(30.dp))
+
+                        Text(text = "Welcome to Historical Flashcards!")
+                        
+                        Spacer(modifier = Modifier.size(30.dp))
+
+                        Text(text = "An app designed to test your knowledge about")
+                        Text(text = "some historical facts in the form of Flashcards.")
+                        Text(text = "Press the 'Next' Button to begin")
+                        
+                        Spacer(modifier = Modifier.size(30.dp))
 
                         Button(
                             onClick = {
